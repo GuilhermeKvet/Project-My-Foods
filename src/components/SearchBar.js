185d5724ecb5ff321ba865e57506a672455288c3
portuga <joaopedromateus6@gmail.com>
2022-07-14 19:33:03 -0300

    Requisito 10

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -11,6 +11,40 @@ function SearchBar() {
           data-testid="search-input"
         />
       </label>
+
+      <label htmlFor="radioIndredients">
+        <input
+          id="radioIndredients"
+          name="search"
+          type="radio"
+          data-testid="ingredient-search-radio"
+        />
+        Ingredients
+      </label>
+
+      <label htmlFor="radioName">
+        <input
+          id="radioName"
+          type="radio"
+          name="search"
+          data-testid="name-search-radio"
+        />
+        Name
+      </label>
+
+      <label htmlFor="radioFisrt">
+        <input
+          type="radio"
+          name="search"
+          id="radioFisrt"
+          data-testid="first-letter-search-radio"
+        />
+        First Letter
+      </label>
+
+      <button type="button" data-testid="exec-search-btn">
+        Search
+      </button>
     </div>
   );
 }
