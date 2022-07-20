f44787c8ac0d5116c03071376d253af546cf6b6c
portuga <joaopedromateus6@gmail.com>
2022-07-20 11:58:01 -0300

    Favoritando receitas

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -4,6 +4,7 @@ import copy from 'clipboard-copy';
 import Recommendation from '../components/Recommendation';
 import shareIcon from '../images/shareIcon.svg';
 import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
 
 const THREE_MILLISECONDS = 3000;
 
@@ -13,6 +14,65 @@ function RecipesDetails() {
   const id = pathname.split('/')[2];
   const [recipe, setRecipe] = useState({});
   const [isShare, setIsShare] = useState(false);
+  const [isFave, setIsFave] = useState(false);
+
+  useEffect(() => {
+    const favorites = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    if (pathname.includes('/drinks') && favorites) {
+      const isFavorite = favorites.some((favorite) => favorite.id === recipe.idDrink);
+      if (isFavorite) setIsFave(true);
+    } else if (favorites) {
+      const isFavorite = favorites.some((favorite) => favorite.id === recipe.idMeal);
+      if (isFavorite) setIsFave(true);
+    }
+  }, [pathname, recipe.idDrink, recipe.idMeal]);
+
+  const setFavorites = (idRecipe, save) => {
+    const favorites = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    if (favorites) {
+      const isFavorite = favorites.some((favorite) => favorite.id === idRecipe);
+      if (isFavorite) {
+        setIsFave(false);
+        const newFavorites = favorites.filter((favorite) => favorite.id !== idRecipe);
+        localStorage.setItem('favoriteRecipes', JSON.stringify(newFavorites));
+      } else {
+        setIsFave(true);
+        const newFavorites = [...favorites, save];
+        localStorage.setItem('favoriteRecipes', JSON.stringify(newFavorites));
+      }
+    } else {
+      setIsFave(true);
+      localStorage.setItem('favoriteRecipes', JSON.stringify([save]));
+    }
+  };
+
+  const saveRecipe = () => {
+    if (pathname.includes('/drinks')) {
+      const { idDrink, strAlcoholic, strCategory, strDrink, strDrinkThumb } = recipe;
+      const save = {
+        id: idDrink,
+        type: 'drink',
+        nationality: '',
+        category: strCategory,
+        alcoholicOrNot: strAlcoholic,
+        name: strDrink,
+        image: strDrinkThumb,
+      };
+      setFavorites(idDrink, save);
+    } else {
+      const { idMeal, strArea, strCategory, strMeal, strMealThumb } = recipe;
+      const save = {
+        id: idMeal,
+        type: 'food',
+        nationality: strArea,
+        category: strCategory,
+        alcoholicOrNot: '',
+        name: strMeal,
+        image: strMealThumb,
+      };
+      setFavorites(idMeal, save);
+    }
+  };
 
   const copyLink = () => {
     copy(window.location.href);
@@ -72,8 +132,12 @@ function RecipesDetails() {
           <img src={ shareIcon } alt="shareIcon" />
         </button>
         {isShare && <span>Link copied!</span>}
-        <button type="button" data-testid="favorite-btn">
-          <img src={ whiteHeartIcon } alt="whiteHeartIcon" />
+        <button type="button" onClick={ saveRecipe }>
+          <img
+            src={ isFave ? blackHeartIcon : whiteHeartIcon }
+            alt="heartIcon"
+            data-testid="favorite-btn"
+          />
         </button>
         <p data-testid="recipe-category">{strAlcoholic}</p>
         {ingredients.map((ingredient, index) => (
@@ -128,8 +192,12 @@ function RecipesDetails() {
         <img src={ shareIcon } alt="shareIcon" />
       </button>
       {isShare && <span>Link copied!</span>}
-      <button type="button" data-testid="favorite-btn">
-        <img src={ whiteHeartIcon } alt="whiteHeartIcon" />
+      <button type="button" onClick={ saveRecipe }>
+        <img
+          src={ isFave ? blackHeartIcon : whiteHeartIcon }
+          alt="heartIcon"
+          data-testid="favorite-btn"
+        />
       </button>
       <h3 data-testid="recipe-category">{strCategory}</h3>
       {ingredients.map((ingredient, index) => (
