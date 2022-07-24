4fd6db14c116a3e262641e680d0a2073546b286c
portuga <joaopedromateus6@gmail.com>
2022-07-24 04:47:43 -0300

    Testes no componente Header ajustado

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -3,21 +3,34 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
+import meals from  '../../cypress/mocks/meals';
+import drinks from '../../cypress/mocks/drinks';
+import mealCategories from '../../cypress/mocks/mealCategories';
 
 describe('Testando o componente Header', () => {
-  it("Verifica se a um icone de usuario e um de pesquisa", () => {
-    const { history } = renderWithRouter(<App />);
-    history.push('/foods');
+  beforeEach(async () => {
+    global.fetch = jest.fn((url) =>
+    Promise.resolve({
+        json: () => {
+          if (url === 'https://www.themealdb.com/api/json/v1/1/search.php?s=') return Promise.resolve(meals)
+          if (url === 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=') return Promise.resolve(drinks)
+          if (url === 'https://www.themealdb.com/api/json/v1/1/list.php?c=list') return Promise.resolve(mealCategories);
+        },
+      })
+    );
+  })
+
+  it("Verifica se a um icone de usuario e um de pesquisa", async () => {
+    const { history } = renderWithRouter(<App />, '/foods');
+    userEvent.click(await screen.findByRole('button', { name: /search\-icon/i }));
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
+    
     userEvent.click(screen.getByRole('button', { name: /profile/i }));
     expect(history.location.pathname).toBe('/profile');
-
-    userEvent.click(screen.getByRole('button', { name: /search\-icon/i }));
-    expect(screen.getByRole('textbox')).toBeInTheDocument();
   });
   
   it("Verifica se exite um titulo", () => {
-    const { history } = renderWithRouter(<App />);
-    history.push('/foods');
+    renderWithRouter(<App />, '/foods');
     expect(screen.getByRole("heading", { name: /foods/i })).toBeInTheDocument();
   });
 });
